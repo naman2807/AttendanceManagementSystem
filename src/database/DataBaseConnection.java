fdb38b3ade07799cb5757a4624282959fb4bf1a4
naman2807 <namanagarwaldbs438@gmail.com>
2021-06-07 16:03:39 +0530

    Added new DataBaseConnection.java class.

---
+++ src/database/DataBaseConnection.java
@@ -0,0 +1,12 @@
+package database;
+
+/**
+ * Created By: Naman Agarwal
+ * User ID: naman2807
+ * Package Name: database
+ * Project Name: AttendanceManagementSystem
+ * Date: 07-06-2021
+ */
+
+public class DataBaseConnection {
+}
