a42fc60ee6b92287c59214a52f475ca32b8e8d0d
naman2807 <namanagarwaldbs438@gmail.com>
2021-06-07 16:21:31 +0530

    Implemented static connectToDataBase() method.

--- src/database/DataBaseConnection.java
+++ src/database/DataBaseConnection.java
@@ -1,5 +1,9 @@
 package database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * Created By: Naman Agarwal
  * User ID: naman2807
@@ -12,16 +16,22 @@ public final class DataBaseConnection {
     private static final String USER = "root";
     private static final String PASSWORD = "";
     private static final String URL = "jdbc:mysql://localhost:3306/attendance_management";
-    private static final DataBaseConnection connection;
+    private static Connection connection;
 
     private DataBaseConnection(){
     }
 
     public static void connectToDataBase(){
-
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Database Connected");
+        } catch (SQLException throwables) {
+            System.out.println("DataBase connection error");
+            throwables.printStackTrace();
+        }
     }
 
-    public static DataBaseConnection getConnection(){
+    public static Connection getConnection(){
         return connection;
     }
 }
