a948fc314e72bd498c2b8632c6a7204358cc3e30
naman2807 <namanagarwaldbs438@gmail.com>
2021-06-07 15:42:08 +0530

    Created a new Candidate.java class.

---
+++ src/data/Candidate.java
@@ -0,0 +1,12 @@
+package data;
+
+/**
+ * Created By: Naman Agarwal
+ * User ID: naman2807
+ * Package Name: data
+ * Project Name: AttendanceManagementSystem
+ * Date: 07-06-2021
+ */
+
+public class Candidate {
+}
