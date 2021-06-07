33e90ee2468d442b4ef5f46600c8fa903e00fb7f
naman2807 <namanagarwaldbs438@gmail.com>
2021-06-07 16:38:17 +0530

    Implemented updateAttendanceDateQuery() method in SQLQueries.java

--- src/database/SQLQueries.java
+++ src/database/SQLQueries.java
@@ -28,6 +28,6 @@ public class SQLQueries {
     }
 
     public static String updateAttendanceDateQuery(){
-
+        return "UPDATE " + ATTENDANCE_TABLE + " SET " + DATE + " = ? WHERE " + ID + " = ?";
     }
 }
