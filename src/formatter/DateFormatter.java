bfb52cd41626518a02416ef976601f7687e10f85
naman2807 <namanagarwaldbs438@gmail.com>
2021-06-08 12:20:36 +0530

    Created a new local dateFormatter variable of DateTimeFormatter type.

--- src/formatter/DateFormatter.java
+++ src/formatter/DateFormatter.java
@@ -1,6 +1,7 @@
 package formatter;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 /**
@@ -16,6 +17,7 @@ public class DateFormatter {
 
     public static String getCurrentFormattedDate(){
         LocalDate date = LocalDate.now();
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd, MMMM yyyy");
         return null;
     }
 }
