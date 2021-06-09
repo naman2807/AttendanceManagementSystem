3a3299f204e31d5117e5e2f5062e5f23b01c5968
naman2807 <namanagarwaldbs438@gmail.com>
2021-06-09 17:24:59 +0530

    Created a MainController.java class.

---
+++ src/gui/MainController.java
@@ -0,0 +1,12 @@
+package gui;
+
+/**
+ * Created By: Naman Agarwal
+ * User ID: naman2807
+ * Package Name: gui
+ * Project Name: AttendanceManagementSystem
+ * Date: 09-06-2021
+ */
+
+public class MainController {
+}
