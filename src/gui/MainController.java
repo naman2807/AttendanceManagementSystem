b7dad96ff2ff9d70d80de773f891788e0e42260a
naman2807 <namanagarwaldbs438@gmail.com>
2021-06-24 13:57:54 +0530

    Set the title of stage.

--- src/gui/MainController.java
+++ src/gui/MainController.java
@@ -59,6 +59,7 @@ public class MainController {
     private void markAttendance() throws IOException {
         Stage stage = new Stage();
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("markattendance.fxml")));
+        stage.setTitle("Mark Attendance");
     }
 
     private void addCandidateToRecord() throws IOException {
