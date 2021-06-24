de9a3eed854d4eba0200e1baddb47669c2d52b17
naman2807 <namanagarwaldbs438@gmail.com>
2021-06-24 13:49:27 +0530

    Created new MarkAttendanceController.java class.

---
+++ src/gui/MarkAttendanceController.java
@@ -0,0 +1,12 @@
+package gui;
+
+/**
+ * Created By: Naman Agarwal
+ * User ID: naman2807
+ * Package Name: gui
+ * Project Name: AttendanceManagementSystem
+ * Date: 24-06-2021
+ */
+
+public class MarkAttendanceController {
+}
