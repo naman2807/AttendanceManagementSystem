08fc4dacd9152060ba30604f20fbd67899bbb694
naman2807 <namanagarwaldbs438@gmail.com>
2021-06-28 10:49:16 +0530

    Declared Stage local field in showAttendancePane() method

--- src/gui/MainController.java
+++ src/gui/MainController.java
@@ -64,7 +64,7 @@ public class MainController {
     }
 
     private void showAttendancePane(){
-
+        Stage stage = new Stage();
     }
 
     private void markAttendance() throws IOException, SQLException {
