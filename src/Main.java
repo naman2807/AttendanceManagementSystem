e73cbb2ddfcd4dee4d4bd310244176978ddf4d99
naman2807 <namanagarwaldbs438@gmail.com>
2021-07-28 09:50:41 +0530

    Created showattendancepiechart.fxml file

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import formatter.DateFormatter;
+import formatter.TimeFormatter;
+
+/**
+ * Created By: Naman Agarwal
+ * User ID: naman2807
+ * Package Name: PACKAGE_NAME
+ * Project Name: AttendanceManagementSystem
+ * Date: 08-06-2021
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(DateFormatter.getCurrentFormattedDate());
+        System.out.println(TimeFormatter.getCurrentTime());
+    }
+}
--- src/formatter/TimeFormatter.java
+++ src/formatter/TimeFormatter.java
@@ -15,7 +15,7 @@ public class TimeFormatter {
 
     public static boolean validateTime(){
         LocalTime time = LocalTime.now();
-        return time.isAfter(LocalTime.of(10,00))
+        return time.isAfter(LocalTime.of(8,30))
                 || time.isBefore(LocalTime.of(8,00));
     }
 
